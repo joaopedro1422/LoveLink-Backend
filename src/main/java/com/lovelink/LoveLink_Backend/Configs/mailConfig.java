33de731852d36fffd14431c534b55cd9ec398e29
joaopedro1422 <jpcros40414@gmail.com>
2025-05-20 19:10:30 -0300

    adc mailsender

---
+++ src/main/java/com/lovelink/LoveLink_Backend/Configs/mailConfig.java
@@ -0,0 +1,29 @@
+package com.lovelink.LoveLink_Backend.Configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class mailConfig {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("lovelinksuport@gmail.com");
+        mailSender.setPassword("jmkxydzgerzluhha");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
