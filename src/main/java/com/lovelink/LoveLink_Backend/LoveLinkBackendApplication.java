48d31f7100508d3383c25b2068eecbc259c9bdee
joaopedro1422 <jpcros40414@gmail.com>
2025-05-20 18:35:57 -0300

    configurando deploy - ports1

--- src/main/java/com/lovelink/LoveLink_Backend/LoveLinkBackendApplication.java
+++ src/main/java/com/lovelink/LoveLink_Backend/LoveLinkBackendApplication.java
@@ -3,10 +3,22 @@ package com.lovelink.LoveLink_Backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class LoveLinkBackendApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(LoveLinkBackendApplication.class, args);
+		SpringApplication app = new SpringApplication(LoveLinkBackendApplication.class);
+
+		Map<String, Object> props = new HashMap<>();
+		String port = System.getenv("PORT");
+		if (port != null) {
+			props.put("server.port", port); // Usa a porta que o Railway define
+		}
+
+		app.setDefaultProperties(props);
+		app.run(args);
 	}
 //add
 }
